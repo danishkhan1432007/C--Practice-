6f3704091668bb1abbfa283bc97d4309353dc256
danishkhan1432007 <danishkhan1432007@gmail.com>
2026-04-09 09:04:57 +0500

    Program of problem 7th

--- Labs/problem7.cpp
+++ Labs/problem7.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 using namespace std;
 int main (){
-    
+    char grade;
+    cout<<"Enter your Grades of the first semester : ";
+    cin>>grade;
+    switch (grade)
+    {
+    case 'A':
+        cout<<"your GPA of the first semester is 4.0 ";
+        break;
+    case 'B':
+        cout<<"your GPA of the first semester is 3.0 ";
+        break;
+    case 'C':
+        cout<<"your GPA of the first semester is 2.0 ";
+        break;
+    case 'D':
+        cout<<"your GPA of the first semester is 1.0 ";
+        break;
+    case 'E':
+        cout<<"your GPA of the first semester is 0.0 ";
+    default:
+        cout<<"The Grade is invalid.";
+        break;
+    }
+    return 0;
 }
